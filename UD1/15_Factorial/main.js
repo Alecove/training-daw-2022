b92c2e42b1912140df03dd4660d2422aba3c4412
DESKTOP-481791G\S2-PC00 <licencias@svalero.com>
2025-09-22 18:59:25 +0200

    ejercicios nuevos

--- UD1/15_Factorial/main.js
+++ UD1/15_Factorial/main.js
@@ -2,7 +2,7 @@
 
 function factorial(n) {
     let resultado = 1;
-    for (let i = 1; i < n; i++) { // <-- off-by-one
+    for (let i = 1; i <= n; i++) { // <-- off-by-one
         resultado *= i;
     }
     return resultado;
